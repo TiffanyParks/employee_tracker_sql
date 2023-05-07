f6bf39c09f7cd71e9d1ccd7f410bfbde054d1b1a
Tiffany <tapintobeauty08@gmail.com>
2023-05-07 18:45:37 -0400

    add create role

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const db = mysql.createConnection({
     database: "employee_tracker"
 })
 
-function mainMenu () {
+function mainMenu() {
     inquirer.prompt([
         {
             type: "list",
@@ -28,54 +28,57 @@ function mainMenu () {
             ]
         }
     ])
-    .then(answer => {
-        if(answer.action == "view all departments") {
-            viewAllDepartments()
-        }
-        if(answer.action == "view all roles") {
-            viewAllRoles()
-        }
-        if(answer.action == "view all employees") {
-            viewAllEmployees()
-        }
-        if(answer.action == "add a department") {
-            createDepartment()
-        }
-    })
+        .then(answer => {
+            if (answer.action == "view all departments") {
+                viewAllDepartments()
+            }
+            if (answer.action == "view all roles") {
+                viewAllRoles()
+            }
+            if (answer.action == "view all employees") {
+                viewAllEmployees()
+            }
+            if (answer.action == "add a department") {
+                createDepartment()
+            }
+            if (answer.action ==  "add a role") {
+                createRole()
+            }
+        })
 }
 
 
-function viewAllDepartments () {
-     // show all departments
-     db.query("SELECT * FROM department", (err, data) => {
-        if(err) console.log(err)
+function viewAllDepartments() {
+    // show all departments
+    db.query("SELECT * FROM department", (err, data) => {
+        if (err) console.log(err)
 
         console.table(data)
         mainMenu()
     })
 }
 
-function viewAllRoles () {
-        // show all roles
-        db.query("SELECT * FROM role", (err, data) => {
-            if(err) console.log(err)
+function viewAllRoles() {
+    // show all roles
+    db.query("SELECT * FROM role", (err, data) => {
+        if (err) console.log(err)
 
-            console.table(data)
-            mainMenu()
-        })
+        console.table(data)
+        mainMenu()
+    })
 }
 
-function viewAllEmployees () {
-        // show all employees
-        db.query("SELECT * FROM employee", (err, data) => {
-            if(err) console.log(err)
+function viewAllEmployees() {
+    // show all employees
+    db.query("SELECT * FROM employee", (err, data) => {
+        if (err) console.log(err)
 
-            console.table(data)
-            mainMenu()
-        })
+        console.table(data)
+        mainMenu()
+    })
 }
 
-function createDepartment () {
+function createDepartment() {
     inquirer.prompt([
         {
             type: "input",
@@ -83,15 +86,44 @@ function createDepartment () {
             message: "What is the name of the new department?"
         }
     ])
-    .then(answers => {
-        db.query("INSERT INTO department (name) VALUES (?);", [answers.department_name],(err, data) => {
-            if(err) console.log(err)
-    
-            console.log("Department has been added!")
-            mainMenu()
-        })
-    })
+        .then(answers => {
+            db.query("INSERT INTO department (name) VALUES (?);", [answers.department_name], (err, data) => {
+                if (err) console.log(err)
 
+                console.log("Department has been added!")
+                mainMenu()
+            })
+        })
+
+}
+
+function createRole() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "title",
+            message: " What is the title of the new role?",
+        },
+        {
+            type: "input",
+            name: "salary",
+            message: " What is the salary of the new role?",
+        },
+        {
+            type: "input",
+            name: "department_id",
+            message: " What is the department_id number of the new role?",
+        },
+    ])
+        .then(answers => {
+            db.query("INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?);",
+                [answers.title, answers.salary, answers.department_id], (err, data) => {
+                    if (err) console.log(err)
+
+                    console.log("Role has been added!")
+                    mainMenu()
+                })
+        })
 }
 
 
